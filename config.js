b14f892e980f9a444f9b38c5326c9056bfabca44
nikitalexstart <nikita@lexstart.in>
2017-12-19 18:24:47 +0530

    FB messenger testing

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     FB_PAGE_TOKEN: 'https://github.com/nikitalexstart/chatbot-facebook-nodejs.git',
     FB_VERIFY_TOKEN: 'lexstart_verify_token_messenger',
-    API_AI_CLIENT_ACCESS_TOKEN: '',
+    API_AI_CLIENT_ACCESS_TOKEN: '6fcb79d044d84c80a3e47257f7b00044',
     FB_APP_SECRET: '929b9eaef3173d0ce475e2495c21625a',
     SERVER_URL: "https://test-lexstart.herokuapp.com/",
 };
